1b583386df27195d14a0bd7ab6a1d25f7a6feab0
Ronen Amon <ronenamon88@gmail.com>
2017-08-17 13:24:12 +0300

    part 2 completed new branch updates: add new fragment "DialogAdd" after this version start with Realm Database completed
    17-8-2017

--- app/src/main/java/com/ronentech/ronen/bucketdrops/ActivityMain.java
+++ app/src/main/java/com/ronentech/ronen/bucketdrops/ActivityMain.java
@@ -2,12 +2,58 @@ package com.ronentech.ronen.bucketdrops;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ActivityMain extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    //Toolbar v7
+    Toolbar mToolbar;
+
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mButton = (Button)findViewById(R.id.btn_add);
+
+        //add a drop button on the main screen
+        mButton.setOnClickListener(this);
+
+
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
+
+
+    }
+
+    //set the background by the Glide plugin
+    private void initBackgroundImage() {
+        ImageView BackgroundIV = (ImageView) findViewById(R.id.iv_background);
+        //Glide.with(this).load("http://goo.gl/gEgYUd").into(imageView);
+        Glide.with(this).load(R.drawable.background).centerCrop().into(BackgroundIV);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        showDialogAdd();
+    }
+
+    private void showDialogAdd() {
+        //create an objcet
+        DialogAdd dialog = new DialogAdd();
+        dialog.show(getSupportFragmentManager(),"Add");
+
     }
 }
---
+++ app/src/main/java/com/ronentech/ronen/bucketdrops/DialogAdd.java
@@ -0,0 +1,56 @@
+package com.ronentech.ronen.bucketdrops;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+/**
+ * Created by ronen on 17/08/2017.
+ */
+
+public class DialogAdd extends DialogFragment {
+
+    private ImageButton mBtnClose;
+    private EditText mInputWhat;
+    private DatePicker mInputWhen;
+    private Button mBtnAdd;
+
+
+
+    public DialogAdd() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog_add,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBtnClose = view.findViewById(R.id.btn_close);
+        mInputWhat = view.findViewById(R.id.et_drop);
+        mInputWhen = view.findViewById(R.id.bpv_date);
+        mBtnAdd = view.findViewById(R.id.btn_add_it);
+
+        mBtnClose.setOnClickListener(mBtnCloseListener);
+
+    }
+
+
+    private View.OnClickListener mBtnCloseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            dismiss();
+        }
+    };
+}
